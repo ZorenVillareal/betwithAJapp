a6082e5b6143991a0ab2389103d4ee12169484c3
Zoren Villareal <zoren.villareal@gmail.com>
2024-03-18 00:34:33 -0700

    installed vite react bootstrap and created navbar

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 MERN full stack app
 MongoDB,Express,React,Node.js
+front end used vite reduxtoolkit and reactbootstrap
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header.jsx';
+
+const App = ()=> {
+  return (
+    <>
+      <Header/>
+        <h1> MERN Auth</h1>
+    </>
+  )
+}
+
+export default App;
